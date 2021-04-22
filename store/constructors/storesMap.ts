e894ee205c2d1cc6eb6f0f1fd9d82aee75a2fe68
Gnarus <g.avet.gnaritas@gmail.com>
2021-04-22 12:03:54 -0400

    deleting bestbuy related stuff

--- store/constructors/storesMap.ts
+++ store/constructors/storesMap.ts
@@ -1,12 +1,10 @@
 import { Page } from "puppeteer";
 import { Store, StoreName } from "../../interface";
 import amazon from "./amazon";
-import bestbuy from "./bestbuy";
 import newegg from "./newegg";
 
 const storesMap: Record<StoreName, (page: Page, itemNumber: string) => Store> = {
     newegg: newegg,
-    // bestbuy: bestbuy,
     amazon: amazon,
 }
 
